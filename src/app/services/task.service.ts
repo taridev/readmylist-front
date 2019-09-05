699bc2651ead8bc3019ad58874512af95a5bea53
VictorGrbt <victor.grebaut@sfr.fr>
2019-09-05 14:02:04 +0200

    AddMethodsTask
    Ajout de methodes getAll, getById...

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import { Task } from '../model/task';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
+  tasks: Task[] =
+  [
+    {
+      id: 1,
+      title: 'tache 1',
+      creationDate: null ,
+      dueDate: null ,
+      done: null ,
+      prioritize: null,
+    },
+    {
+      id: 2,
+      title: 'tache 2',
+      creationDate:  null,
+      dueDate: null,
+      done: null,
+      prioritize: null,
+    }
+  ];
+
   constructor() { }
+
+  getAll() {
+    return this.tasks;
+  }
+
+  getById(id: number) {
+    return this.tasks.find(task => task.id === id);
+  }
+
+  getAllByTitle(title: string) {
+    return this.tasks.filter(task => task.title === title);
+  }
+
+  getAllByTitlePattern(pattern: string) {
+    return this.tasks.filter(task => task.title.indexOf(pattern) >= -1);
+  }
+
 }
