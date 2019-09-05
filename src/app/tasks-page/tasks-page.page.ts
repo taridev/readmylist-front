a4cc5cad8c34c88438cb850eb550dfe6ed53bef1
StevenOddoz <steven.oddoz@live.fr>
2019-09-05 15:20:40 +0200

    patch
    correction d'un précédent commit

--- src/app/tasks-page/tasks-page.page.ts
+++ src/app/tasks-page/tasks-page.page.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import {TaskListService} from '../services/TaskListService';
-=======
 import { TaskListMockService } from '../services/task-list-mock.service';
 import { TaskList } from '../model/task-list';
 import { ActivatedRoute } from '@angular/router';
->>>>>>> d2ee12be521684db3e2911097bdccf87705fbdd4
 
 @Component({
   selector: 'app-tasks-page',
@@ -14,11 +10,6 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class TasksPagePage implements OnInit {
 
-<<<<<<< HEAD
-  constructor(private service: TaskListService) {
-    service.getById(1);
-  }
-=======
   tasklist: TaskList = null
   tasks: TaskList['tasks'] = null;
 
@@ -26,7 +17,6 @@ export class TasksPagePage implements OnInit {
     this.tasklist = this.taskLIstMockService.getById(1);
     this.tasks = this.tasklist.tasks;
    }
->>>>>>> d2ee12be521684db3e2911097bdccf87705fbdd4
 
   ngOnInit() {
   }
