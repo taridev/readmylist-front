f7b73ea93985fac3b22c0db53ab14af735291c49
StevenOddoz <steven.oddoz@live.fr>
2019-09-06 13:56:47 +0200

    Ajout MethodUpdate

--- src/app/services/TaskService.ts
+++ src/app/services/TaskService.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@angular/core';
-import { ITaskService } from './itask.service';
 import { Task } from '../model/task';
+import {environment} from '../../environments/environment';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+const API_URL = environment.apiUrl;
+const HTTP_OPTION = {
+  headers: new HttpHeaders(
+      {
+        'Content-Type': 'application/json',
+      }
+  )
+};
 
 @Injectable({
   providedIn: 'root'
 })
-export class TaskService implements ITaskService {
+export class TaskService {
+  constructor(private http: HttpClient) { }
 
-    getAllByTitlePattern(pattern: string): Task[] {
+  getAllByTitlePattern(pattern: string): Task[] {
     throw new Error('Method not implemented.');
   }
   getById(id: number): Task {
@@ -20,11 +31,22 @@ export class TaskService implements ITaskService {
     throw new Error('Method not implemented.');
   }
   update(task: Task): Task {
-    throw new Error('Method not implemented.');
+    const headers = new HttpHeaders();
+    const url = `${API_URL}/task/update/${task.id}`;
+    headers.append('Accept', 'application/json');
+    headers.append('Content-Type', 'application/json');
+
+    this.http.put(url, task)
+        .subscribe(data =>
+                task = new Task(data)
+            , error => {
+              console.log(error);
+            });
+    return task;
   }
   delete(task: Task) {
     throw new Error('Method not implemented.');
   }
 
-  constructor() { }
+
 }
