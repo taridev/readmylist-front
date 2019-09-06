0ce14de688d22411c6ef41cdbdc8c09142b84558
ioanapana <iona.antoneseipana@cgi.com>
2019-09-06 09:23:08 +0200

    FEAT: login page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'tasks-page', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'tasks-page', loadChildren: './tasks-page/tasks-page.module#TasksPagePageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+
 
 ];
 
